8c7a03500f222014f179b86a9c859969946b5154
jaesjeon <jaesjeon@student.42seoul.kr>
2022-02-08 21:58:18 +0900

    selection sorting making_need debug

---
+++ selection_sort/main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int	ft_atoi(const char *str);
+void	selection_sort(int *num_list, int len);
+
+int	main(int argc, char *argv[])
+{
+	int	*num_list;
+	int	len;
+	int	idx;
+
+	len = argc - 1;
+	idx = 0;
+	num_list = (int *)malloc(sizeof(int) * (argc - 1));
+	while (--argc > 0)
+	{
+		num_list[idx] = ft_atoi(argv[idx + 1]);
+		idx++;
+	}
+	printf("Original number list\n");
+	printf(">>>>>>>>>>>>>>>>>>>\n");
+	while (argc < idx)
+	{
+		printf("%d ", num_list[argc++]);
+	}
+	printf("\n");
+	printf("<<<<<<<<<<<<<<<<<<<\n");
+	selection_sort(num_list, len);
+	printf("sorted number list\n");
+	printf(">>>>>>>>>>>>>>>>>>>\n");
+	argc = 0;
+	while (argc < idx)
+	{
+		printf("%d ", num_list[argc++]);
+	}
+	printf("\n");
+	printf("<<<<<<<<<<<<<<<<<<<\n");
+	return (0);
+}
---
+++ selection_sort/selection_sort.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   selection_sort.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaesjeon <jaesjeon@student.42seoul.>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/08 20:49:23 by jaesjeon          #+#    #+#             */
+/*   Updated: 2022/02/08 21:57:36 by jaesjeon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+void	selection_sort(int *num_list, int len)
+{
+	int		compare_count;
+	int		swap_count;
+	int		primary_i;
+	int		sub_i;
+	int		tmp;
+	int		min;
+	int		min_idx;
+
+	compare_count = 0;
+	swap_count = 0;
+	primary_i = 0;
+	while (primary_i < len)
+	{
+		min = 2147483647;
+		sub_i = primary_i;
+		while (sub_i < len - primary_i)
+		{
+			if (num_list[sub_i] < min)
+			{
+				min = num_list[sub_i];
+				min_idx = sub_i;
+			}
+			compare_count++;
+			sub_i++;
+		}
+		if (num_list[primary_i] != num_list[min_idx])
+		{
+			tmp = num_list[primary_i];
+			num_list[primary_i] = num_list[min_idx];
+			num_list[min_idx] = tmp;
+			swap_count++;
+		}
+		primary_i++;
+	}
+	printf("compare count = %d\n", compare_count);
+	printf("swap count = %d\n", swap_count);
+}
