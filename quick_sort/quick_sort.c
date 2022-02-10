e1579bac251fc07f40165edfefaa54855c943578
jaesjeon <jaesjeon@student.42seoul.kr>
2022-02-10 21:51:32 +0900

    quick_sort doing

---
+++ quick_sort/quick_sort.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   quick_sort.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaesjeon <jaesjeon@student.42seoul.>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/10 19:40:44 by jaesjeon          #+#    #+#             */
+/*   Updated: 2022/02/10 21:51:10 by jaesjeon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	quick_sort(int *num_list, int start, int end)
+{
+	int	compare_count = 0;
+	int	swap_count = 0;
+	int	primary_idx = start + 1;
+	int	sub_idx = end;
+
+	if (start >= end)
+		return ;
+	while (
+}
