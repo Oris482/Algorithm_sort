e10cd5050844006c508a57383ca34a654dec1c69
jaesjeon <jaesjeon@student.42seoul.kr>
2022-02-15 21:53:28 +0900

    quick sort working...

--- quick_sort/quick_sort.c
+++ quick_sort/quick_sort.c
@@ -6,7 +6,7 @@
 /*   By: jaesjeon <jaesjeon@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/10 19:40:44 by jaesjeon          #+#    #+#             */
-/*   Updated: 2022/02/10 21:51:10 by jaesjeon         ###   ########.fr       */
+/*   Updated: 2022/02/15 21:52:58 by jaesjeon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,28 @@ void	quick_sort(int *num_list, int start, int end)
 	int	swap_count = 0;
 	int	primary_idx = start + 1;
 	int	sub_idx = end;
+	int	pivot_idx = start;
+	int	tmp;
 
 	if (start >= end)
 		return ;
-	while (
+	while (num_list[pivot_idx] < num_list[primary_idx])
+		primary_idx++;
+	while ((num_list[pivot_idx] > num_list[sub_idx]) && (sub_idx > pivot_idx))
+		sub_idx--;
+	if (sub_idx <= primary_idx)
+	{
+		tmp = num_list[pivot_idx];
+		num_list[pivot_idx] = num_list[sub_idx];
+		num_list[sub_idx] = tmp;
+		quick_sort(num_list, pivot_idx, sub_idx - 1);
+	}
+	else
+	{
+		tmp = num_list[primary_idx];
+		num_list[primary_idx] = num_list[sub_idx];
+		num_list[sub_idx] = tmp;
+		quick_sort(num_list, sub_idx + 1, end);
+	}
+	return ;
 }
